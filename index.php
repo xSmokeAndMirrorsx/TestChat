0eff465b530652f067797f998968a67be0845048
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-03 18:58:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,8 @@ function loginForm(){
  
 ?>
  
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
