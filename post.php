95d445cdfc41d184c2e86fd1982adb4c46bd00d6
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-03 18:21:04 -0400

    Update post.php

--- post.php
+++ post.php
@@ -5,10 +5,10 @@ if(isset($_SESSION['name'])){
     $idnum = $_POST['idnum'];
     $likeID = "like_".$idnum;
     $likebuttonID = "likeB_".$idnum;
-    $commentID = "comment_".$idnum."_txt";
-    $commentbuttonID = "comment_".$idnum;
+    $commentID = "comment_".$idnum;
+    $commentbuttonID = "commentB_".$idnum;
      
-    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><input name='".$idnum."' type='submit' id='likeButton' value='Like' />  Likes: <b id='".$likeID."' value='0'></b></span></p><br></div>";
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><input name='like' type='submit' id='".$likebuttonID."' value='Like' />  Likes: <b id='".$likeID."' value='0'></b></span></p><br></div><script type='text/javascript'>$('#".$likebuttonID."').click(function(){var likeCount=parseInt($('#".$likeID."').val(), 10); likeCount=likeCount+1; $('#".$likeID."').val(likeCount); });</script>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
