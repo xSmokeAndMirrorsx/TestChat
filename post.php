b273623bdc8aa80d7ae43cf38f93978c284124bc
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-19 18:40:43 -0400

    Update post.php

--- post.php
+++ post.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['name'])){
     $commentID = "comment_".$idnum;
     $commentbuttonID = "commentB_".$idnum;
      
-    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><button id='".$likebuttonID."' onclick='add_likes(".$likeID.")'> Like </button> -- Likes: <b id='".$likeID."'>0</b></span></p><br></div>";
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><button id=".$likebuttonID." onclick='add_likes(".$likeID.")'> Like </button> -- (".$likeID.") Likes: <b id=".$likeID.">0</b></span></p><br></div>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
