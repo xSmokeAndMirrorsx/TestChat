b3a526ec2aeec4a87f26853f18153e34139c87da
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-19 18:12:20 -0400

    Update post.php

--- post.php
+++ post.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['name'])){
     $commentID = "comment_".$idnum;
     $commentbuttonID = "commentB_".$idnum;
      
-    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><input name='like' type='submit' id='".$likebuttonID."' value='Like' />  Likes: <b id='".$likeID."'>0</b></span></p><br></div><script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script><script type='text/javascript'> $(document).ready(function () { $('#".$likebuttonID."').click(function(){var likeCount=parseInt($('#".$likeID."').innerText, 10); likeCount=likeCount+1; $('#".$likeID."').innerText = likeCount; }); });</script>";
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<p><span><button id='".$likebuttonID."' onclick='add_likes(".$likeID.")'> Like </button> -- Likes: <b id='".$likeID."'>0</b></span></p><br></div>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
